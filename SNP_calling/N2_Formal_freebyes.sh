05f50f5cf6f45425073a06a47534b93e613943e4
RohitKapila <105384374+RohitKapila@users.noreply.github.com>
2024-11-05 12:40:42 -0500

    Update FreeBayes command output file extensions and fix errors

--- SNP_calling/N2_Formal_freebyes.sh
+++ SNP_calling/N2_Formal_freebyes.sh
@@ -1,10 +1,7 @@
 module load freebayes-1.3.2
 
-freebayes -f N2_refrence.fna -F 0.01 -C 1 --pooled-continuous  N2_1Formal_SR_Sorted_alignment.bam >N2_1Formal_Conti_var_nofilter.v
-cf
-freebayes -f N2_refrence.fna -F 0.01 -C 1 --pooled-continuous  N2_2Formal_SR_Sorted_alignment.bam >N2_2Formal_Conti_var_nofilter.v
-cf
-freebayes -f N2_refrence.fna -F 0.01 -C 1 --pooled-continuous  N2_3Formal_SR_Sorted_alignment.bam >N2_3Formal_Conti_var_nofilter.v
-cf
-freebayes -f N2_refrence.fna -F 0.01 -C 1 --pooled-continuous  N2_4Formal_SR_Sorted_alignment.bam >N2_4Formal_Conti_var_nofilter.v
-cf
+freebayes -f N2_refrence.fna -F 0.05 -C 2 --pooled-continuous  N2_1Formal_SR_Sorted_alignment.bam >N2_1Formal_Conti_var.vcf
+freebayes -f N2_refrence.fna -F 0.05 -C 2 --pooled-continuous  N2_2Formal_SR_Sorted_alignment.bam >N2_2Formal_Conti_var.vcf
+freebayes -f N2_refrence.fna -F 0.05 -C 2 --pooled-continuous  N2_3Formal_SR_Sorted_alignment.bam >N2_3Formal_Conti_var.vcf
+freebayes -f N2_refrence.fna -F 0.05 -C 2 --pooled-continuous  N2_4Formal_SR_Sorted_alignment.bam >N2_4Formal_Conti_var.vcf
+
