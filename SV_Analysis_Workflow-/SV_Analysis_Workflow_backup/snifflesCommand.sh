f3463981297fb41ad43e624cf81ee8fa8cfdca55
RohitKapila <105384374+RohitKapila@users.noreply.github.com>
2025-04-14 12:21:31 -0400

    Re-add SV_Analysis_Workflow- as normal folder (fix broken submodule)

---
+++ SV_Analysis_Workflow-/SV_Analysis_Workflow_backup/snifflesCommand.sh
@@ -0,0 +1,10 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2822
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fnil\fcharset0 .AppleSystemUIFontMonospaced-Regular;}
+{\colortbl;\red255\green255\blue255;\red24\green26\blue30;\red110\green120\blue134;}
+{\*\expandedcolortbl;;\cssrgb\c12157\c13725\c15686;\cssrgb\c50588\c54510\c59608\c12157;}
+\paperw11900\paperh16840\margl1440\margr1440\vieww37900\viewh21300\viewkind0
+\deftab720
+\pard\pardeftab720\sa320\partightenfactor0
+
+\f0\fs27\fsmilli13600 \cf2 \cb3 \expnd0\expndtw0\kerning0
+sniffles -i mapped_input.bam -v output.vcf}
